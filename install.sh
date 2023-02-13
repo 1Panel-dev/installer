0d88f11c6c2b9095b7425bc0d87c8a33dad89ea6
wanghe-fit2cloud <wanghe@fit2cloud.com>
2023-02-13 18:30:52 +0800

    feat: 优化在线安装脚本

--- install.sh
+++ install.sh
@@ -10,14 +10,6 @@ function log() {
     echo -e "${message}" 2>&1 | tee -a ${CURRENT_DIR}/install.log
 }
 
-args=$@
-os=`uname -a`
-docker_config_folder="/etc/docker"
-
-if [ -f /usr/bin/1pctl ]; then
-    1pctl uninstall
-fi
-
 echo
 cat << EOF
  ██╗    ██████╗  █████╗ ███╗   ██╗███████╗██╗     
@@ -30,115 +22,147 @@ EOF
 
 echo -e "======================= 开始安装 =======================" 2>&1 | tee -a ${CURRENT_DIR}/install.log
 
-#Set 1Panel installation directory
-if read -t 120 -p "设置 1Panel 安装目录,默认 /opt: " PANEL_BASE_DIR;then
-if [ "$PANEL_BASE_DIR" != "" ];then
-    echo "你选择的安装路径为 $PANEL_BASE_DIR"
-    if [ ! -d $PANEL_BASE_DIR ];then
-        mkdir -p $PANEL_BASE_DIR
-    fi
-else
-    PANEL_BASE_DIR=/opt
-    echo "你选择的安装路径为 $PANEL_BASE_DIR"
-fi
-else
-    PANEL_BASE_DIR=/opt
-    echo "(设置超时，使用默认安装路径 /opt)"
-fi
-
-#Install docker & docker-compose
-##Install Latest Stable Docker Release
-if which docker >/dev/null; then
-    log "检测到 Docker 已安装，跳过安装步骤"
-    log "启动 Docker "
-    service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
-else
-    log "... 在线安装 docker"
-    curl -fsSL https://get.docker.com -o get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
-    sudo sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
-    log "... 启动 docker"
-    systemctl enable docker; systemctl daemon-reload; service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
-
-    if [ ! -d "$docker_config_folder" ];then
-        mkdir -p "$docker_config_folder"
-    fi
-
-    docker version >/dev/null
-    if [ $? -ne 0 ]; then
-        log "docker 安装失败"
+function Prepare_System(){
+    is64bit=`getconf LONG_BIT`
+    if [ $is64bit != '64' ]; then
+        echo "错误：32位系统不支持安装 1Panel Linux 面板，请更换64位系统安装。"
         exit 1
-    else
-        log "docker 安装成功"
     fi
-fi
 
-##Install Latest Stable Docker Compose Release
-docker-compose version >/dev/null
-if [ $? -ne 0 ]; then
-    log "... 在线安装 docker-compose"
-    curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
-    chmod +x /usr/local/bin/docker-compose
-    ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+    isInstalled=`systemctl status 1panel 2>&1 | grep Active`
+    if [ $isInstalled != "" ]; then
+        echo "错误：1Panel Linux 面板已安装，请勿重复安装。"
+        exit 1
+    fi
+}
 
+function Set_Dir(){
+    if read -t 120 -p "设置 1Panel 安装目录,默认 /opt: " PANEL_BASE_DIR;then
+        if [ "$PANEL_BASE_DIR" != "" ];then
+            echo "你选择的安装路径为 $PANEL_BASE_DIR"
+            if [ ! -d $PANEL_BASE_DIR ];then
+                mkdir -p $PANEL_BASE_DIR
+            fi
+        else
+            PANEL_BASE_DIR=/opt
+            echo "你选择的安装路径为 $PANEL_BASE_DIR"
+        fi
+    else
+        PANEL_BASE_DIR=/opt
+        echo "(设置超时，使用默认安装路径 /opt)"
+    fi
+}
+
+function Install_Docker(){
+    if which docker >/dev/null; then
+        log "检测到 Docker 已安装，跳过安装步骤"
+        log "启动 Docker "
+        service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
+    else
+        log "... 在线安装 docker"
+        curl -fsSL https://get.docker.com -o get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
+        sudo sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
+        log "... 启动 docker"
+        systemctl enable docker; systemctl daemon-reload; service docker start 2>&1 | tee -a ${CURRENT_DIR}/install.log
+
+        docker_config_folder="/etc/docker"
+        if [ ! -d "$docker_config_folder" ];then
+            mkdir -p "$docker_config_folder"
+        fi
+
+        docker version >/dev/null
+        if [ $? -ne 0 ]; then
+            log "docker 安装失败"
+            exit 1
+        else
+            log "docker 安装成功"
+        fi
+    fi
+}
+
+function Install_Compose(){
     docker-compose version >/dev/null
     if [ $? -ne 0 ]; then
-        log "docker-compose 安装失败"
-        exit 1
+        log "... 在线安装 docker-compose"
+        curl -L https://get.daocloud.io/docker/compose/releases/download/1.29.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
+        chmod +x /usr/local/bin/docker-compose
+        ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+        docker-compose version >/dev/null
+        if [ $? -ne 0 ]; then
+            log "docker-compose 安装失败"
+            exit 1
+        else
+            log "docker-compose 安装成功"
+        fi
     else
-        log "docker-compose 安装成功"
+        log "检测到 Docker Compose 已安装，跳过安装步骤"
     fi
-else
-    log "检测到 Docker Compose 已安装，跳过安装步骤"
-fi
+}
 
-if which firewall-cmd >/dev/null; then
-    if systemctl is-active firewalld &>/dev/null ;then
-        log "防火墙端口开放"
-        firewall-cmd --zone=public --add-port=9999/tcp --permanent
-        firewall-cmd --reload
-    else
-        log "防火墙未开启，忽略端口开放"
+function Set_Firewall(){
+    if which firewall-cmd >/dev/null; then
+        if systemctl is-active firewalld &>/dev/null ;then
+            log "防火墙端口开放"
+            firewall-cmd --zone=public --add-port=9999/tcp --permanent
+            firewall-cmd --reload
+        else
+            log "防火墙未开启，忽略端口开放"
+        fi
     fi
-fi
+}
 
-log "配置 1Panel Service"
+function Init_Panel(){
+    log "配置 1Panel Service"
 
-RUN_BASE_DIR=$PANEL_BASE_DIR/1panel
-mkdir -p ${RUN_BASE_DIR}
+    RUN_BASE_DIR=$PANEL_BASE_DIR/1panel
+    mkdir -p $RUN_BASE_DIR
+    rm -rf $RUN_BASE_DIR/*
 
-cd ${CURRENT_DIR}
+    cd ${CURRENT_DIR}
 
-sed -i -e "s#BASE_DIR=.*#BASE_DIR=${PANEL_BASE_DIR}#g" ./1pctl
-\cp 1pctl /usr/local/bin && chmod +x /usr/local/bin/1pctl
-if [ ! -f /usr/bin/1pctl ]; then
-    ln -s /usr/local/bin/1pctl /usr/bin/1pctl 2>/dev/null
-fi
-
-cp ./1panel/conf/1panel.service /etc/systemd/system/1panel.service
-
-\cp ./1panel/bin/1panel /usr/local/bin && chmod +x /usr/local/bin/1panel
-if [ ! -f /usr/bin/1panel ]; then
-    ln -s /usr/local/bin/1panel /usr/bin/1panel 2>/dev/null
-fi
-
-systemctl enable 1panel; systemctl daemon-reload 2>&1 | tee -a ${CURRENT_DIR}/install.log
-
-log "启动服务"
-1pctl start | tee -a ${CURRENT_DIR}/install.log
-1pctl status 2>&1 | tee -a ${CURRENT_DIR}/install.log
-
-for b in {1..30}
-do
-    sleep 3
-    service_status=`systemctl status 1panel.service | grep Active`
-    if [[ $service_status == *running* ]];then
-        log "服务启动成功!"
-        break;
-    else
-        log "服务启动出错!"
-        exit 1
+    cp ./1panel /usr/local/bin && chmod +x /usr/local/bin/1panel
+    if [ ! -f /usr/bin/1panel ]; then
+        ln -s /usr/local/bin/1panel /usr/bin/1panel 2>/dev/null
     fi
-done
 
-echo -e "======================= 安装完成 =======================\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log
-echo -e "请通过以下方式访问:\n URL: http://\$LOCAL_IP:9999" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+    sed -i -e "s#BASE_DIR=.*#BASE_DIR=${PANEL_BASE_DIR}#g" ./1pctl
+    cp ./1pctl /usr/local/bin && chmod +x /usr/local/bin/1pctl
+    if [ ! -f /usr/bin/1pctl ]; then
+        ln -s /usr/local/bin/1pctl /usr/bin/1pctl 2>/dev/null
+    fi
+
+    cp ./1panel.service /etc/systemd/system
+
+    systemctl enable 1panel; systemctl daemon-reload 2>&1 | tee -a ${CURRENT_DIR}/install.log
+
+    log "启动服务"
+    1pctl start | tee -a ${CURRENT_DIR}/install.log
+    1pctl status 2>&1 | tee -a ${CURRENT_DIR}/install.log
+
+    for b in {1..30}
+    do
+        sleep 3
+        service_status=`systemctl status 1panel 2>&1 | grep Active`
+        if [[ $service_status == *running* ]];then
+            log "服务启动成功!"
+            break;
+        else
+            log "服务启动出错!"
+            exit 1
+        fi
+    done
+
+    echo -e "======================= 安装完成 =======================\n" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+    echo -e "请通过以下方式访问:\n URL: http://\$LOCAL_IP:9999" 2>&1 | tee -a ${CURRENT_DIR}/install.log
+}
+
+function main(){
+    Prepare_System
+    Set_Dir
+    Install_Docker
+    Install_Compose
+    Set_Firewall
+    Init_Panel
+}
+main
--- quick_start.sh
+++ quick_start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 #Install Latest Stable 1Panel Release
 
-git_urls=('gitee.com' 'github.com')
+osCheck=`uname -a`
+if [[ $osCheck =~ 'x86_64' ]];then
+    architecture="amd64"
+elif [[ $osCheck =~ 'arm64' ]] || [[ $osCheck =~ 'aarch64' ]];then
+    architecture="arm64"
+else
+    echo "暂不支持的系统架构，请参阅官方文档，选择受支持的系统。"
+    exit 1
+fi
 
+git_urls=('gitee.com' 'github.com')
 if [[ -x "$(command -v python)" ]];then
     py_cmd='python'
-elif [[ -x "$(command -v python3)" ]]; then
+elif [[ -x "$(command -v python3)" ]];then
     py_cmd='python3'
 else
     git_urls=('github.com')
@@ -39,30 +48,16 @@ fi
 
 echo "使用下载服务器 ${server_url}"
 
-os=`uname -a`
-if [[ $os =~ 'x86_64' ]];then
-    architecture="amd64"
-elif [[ $os =~ 'arm64' ]] || [[ $os =~ 'aarch64' ]];then
-    architecture="arm64"
-else
-  	echo "暂不支持的系统架构，请参阅官方文档，选择受支持的系统。"
-  	exit 1
-fi
-
-echo "服务器系统架构 ${architecture}"
-
-bin_file_name="1panel-linux-${architecture}"
 if [[ "${server_url}" == "gitee.com" ]];then
     owner='wanghe-fit2cloud'
     repo='1Panel'
     gitee_release_content=$(curl -s https://gitee.com/api/v5/repos/${owner}/${repo}/releases/latest)
-    VERSION=$($py_cmd -c "import json; obj=json.loads('$gitee_release_content'); print(obj['tag_name']);")
-	bin_download_url="https://1panel.oss-cn-hangzhou.aliyuncs.com/releases/${VERSION}/${bin_file_name}"
+    export LC_ALL="en_US.utf8"
+	VERSION=$($py_cmd -c "import json; obj=json.loads('$gitee_release_content', strict=False); print(obj['tag_name']);")
 else
 	owner='wanghe-fit2cloud'
 	repo='1Panel'
 	VERSION=$(curl -s https://api.github.com/repos/${owner}/${repo}/releases/latest | grep -e "\"tag_name\"" | sed -r 's/.*: "(.*)",/\1/')
-    bin_download_url="https://${server_url}/${owner}/${repo}/releases/download/${VERSION}/${bin_file_name}"
 fi
 
 if [[ "x${VERSION}" == "x" ]];then
@@ -72,7 +67,7 @@ fi
 
 echo "开始下载 1Panel ${VERSION} 版本在线安装包"
 
-package_file_name="1panel-online-installer-${VERSION}.tar.gz"
+package_file_name="1panel-${VERSION}-linux-${architecture}.tar.gz"
 package_download_url="https://${server_url}/${owner}/${repo}/releases/download/${VERSION}/${package_file_name}"
 
 echo "安装包下载地址： ${package_download_url}"
@@ -89,16 +84,6 @@ if [ $? != 0 ];then
 	rm -f ${package_file_name}
 	exit 1
 fi
-
-echo "二进制文件下载地址： ${bin_download_url}"
-
-cd 1panel-online-installer-${VERSION}
-curl -LOk -o ${bin_file_name} ${bin_download_url}
-if [ ! -f ${bin_file_name} ];then
-	echo "下载二进制文件失败，请稍候重试。"
-	rm -f ${bin_file_name}
-	exit 1
-fi
-mv ${bin_file_name} ./1panel/bin/1panel
+cd 1panel-${VERSION}-linux-${architecture}
 
 /bin/bash install.sh
