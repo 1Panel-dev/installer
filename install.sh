bc280e5b3c1e52b687cc369b633fa6008ad06f68
gcsong023 <66949363+gcsong023@users.noreply.github.com>
2024-05-25 00:50:30 +0800

    Update install.sh
    尝试匹配systemd与busybox环境下共用。

--- install.sh
+++ install.sh
@@ -65,44 +65,75 @@ function Install_Docker(){
         systemctl start docker 2>&1 | tee -a ${CURRENT_DIR}/install.log
     else
         log "... 在线安装 docker"
+        if  command -v opkg &>/dev/null;then
+            opkg update
+            opkg install luci-i18n-dockerman-zh-cn
+            opkg install zoneinfo-asia
+            service system restart
+        else
+       
+            if [[ $(curl -s ipinfo.io/country) == "CN" ]]; then
+                sources=(
+                    "https://mirrors.aliyun.com/docker-ce"
+                    "https://mirrors.tencent.com/docker-ce"
+                    "https://mirrors.163.com/docker-ce"
+                    "https://mirrors.cernet.edu.cn/docker-ce"
+                )
 
-        if [[ $(curl -s ipinfo.io/country) == "CN" ]]; then
-            sources=(
-                "https://mirrors.aliyun.com/docker-ce"
-                "https://mirrors.tencent.com/docker-ce"
-                "https://mirrors.163.com/docker-ce"
-                "https://mirrors.cernet.edu.cn/docker-ce"
-            )
+                get_average_delay() {
+                    local source=$1
+                    local total_delay=0
+                    local iterations=3
 
-            get_average_delay() {
-                local source=$1
-                local total_delay=0
-                local iterations=3
+                    for ((i = 0; i < iterations; i++)); do
+                        delay=$(curl -o /dev/null -s -w "%{time_total}\n" "$source")
+                        total_delay=$(awk "BEGIN {print $total_delay + $delay}")
+                    done
 
-                for ((i = 0; i < iterations; i++)); do
-                    delay=$(curl -o /dev/null -s -w "%{time_total}\n" "$source")
-                    total_delay=$(awk "BEGIN {print $total_delay + $delay}")
+                    average_delay=$(awk "BEGIN {print $total_delay / $iterations}")
+                    echo "$average_delay"
+                }
+
+                min_delay=${#sources[@]}
+                selected_source=""
+
+                for source in "${sources[@]}"; do
+                    average_delay=$(get_average_delay "$source")
+
+                    if (( $(awk 'BEGIN { print '"$average_delay"' < '"$min_delay"' }') )); then
+                        min_delay=$average_delay
+                        selected_source=$source
+                    fi
                 done
 
-                average_delay=$(awk "BEGIN {print $total_delay / $iterations}")
-                echo "$average_delay"
-            }
+                if [ -n "$selected_source" ]; then
+                    echo "选择延迟最低的源 $selected_source，延迟为 $min_delay 秒"
+                    export DOWNLOAD_URL="$selected_source"
+                    curl -fsSL "https://get.docker.com" -o get-docker.sh
+                    sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
 
-            min_delay=${#sources[@]}
-            selected_source=""
+                    log "... 启动 docker"
+                    systemctl enable docker; systemctl daemon-reload; systemctl start docker 2>&1 | tee -a ${CURRENT_DIR}/install.log
 
-            for source in "${sources[@]}"; do
-                average_delay=$(get_average_delay "$source")
+                    docker_config_folder="/etc/docker"
+                    if [[ ! -d "$docker_config_folder" ]];then
+                        mkdir -p "$docker_config_folder"
+                    fi
 
-                if (( $(awk 'BEGIN { print '"$average_delay"' < '"$min_delay"' }') )); then
-                    min_delay=$average_delay
-                    selected_source=$source
+                    docker version >/dev/null 2>&1
+                    if [[ $? -ne 0 ]]; then
+                        log "docker 安装失败"
+                        exit 1
+                    else
+                        log "docker 安装成功"
+                    fi
+                else
+                    log "无法选择源进行安装"
+                    exit 1
                 fi
-            done
-
-            if [ -n "$selected_source" ]; then
-                echo "选择延迟最低的源 $selected_source，延迟为 $min_delay 秒"
-                export DOWNLOAD_URL="$selected_source"
+            else
+                log "非中国大陆地区，无需更改源"
+                export DOWNLOAD_URL="https://download.docker.com"
                 curl -fsSL "https://get.docker.com" -o get-docker.sh
                 sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
 
@@ -121,30 +152,6 @@ function Install_Docker(){
                 else
                     log "docker 安装成功"
                 fi
-            else
-                log "无法选择源进行安装"
-                exit 1
-            fi
-        else
-            log "非中国大陆地区，无需更改源"
-            export DOWNLOAD_URL="https://download.docker.com"
-            curl -fsSL "https://get.docker.com" -o get-docker.sh
-            sh get-docker.sh 2>&1 | tee -a ${CURRENT_DIR}/install.log
-
-            log "... 启动 docker"
-            systemctl enable docker; systemctl daemon-reload; systemctl start docker 2>&1 | tee -a ${CURRENT_DIR}/install.log
-
-            docker_config_folder="/etc/docker"
-            if [[ ! -d "$docker_config_folder" ]];then
-                mkdir -p "$docker_config_folder"
-            fi
-
-            docker version >/dev/null 2>&1
-            if [[ $? -ne 0 ]]; then
-                log "docker 安装失败"
-                exit 1
-            else
-                log "docker 安装成功"
             fi
         fi
     fi
@@ -154,18 +161,21 @@ function Install_Compose(){
     docker-compose version >/dev/null 2>&1
     if [[ $? -ne 0 ]]; then
         log "... 在线安装 docker-compose"
-        
-        arch=$(uname -m)
-		if [ "$arch" == 'armv7l' ]; then
-			arch='armv7'
-		fi
-		curl -L https://resource.fit2cloud.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s | tr A-Z a-z)-$arch -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
-        if [[ ! -f /usr/local/bin/docker-compose ]];then
-            log "docker-compose 下载失败，请稍候重试"
-            exit 1
-        fi
-        chmod +x /usr/local/bin/docker-compose
-        ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+        if which opkg &>/dev/null;then
+            opkg update || log "软件包更新失败，请检查网络或稍后重试"
+            opkg install docker-compose
+        else
+            arch=$(uname -m)
+            if [ "$arch" == 'armv7l' ]; then
+                arch='armv7'
+            fi
+            curl -L https://resource.fit2cloud.com/docker/compose/releases/download/v2.22.0/docker-compose-$(uname -s | tr A-Z a-z)-$arch -o /usr/local/bin/docker-compose 2>&1 | tee -a ${CURRENT_DIR}/install.log
+            if [[ ! -f /usr/local/bin/docker-compose ]];then
+                log "docker-compose 下载失败，请稍候重试"
+                exit 1
+            fi
+            chmod +x /usr/local/bin/docker-compose
+            ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
         docker-compose version >/dev/null 2>&1
         if [[ $? -ne 0 ]]; then
@@ -379,8 +389,8 @@ function main(){
     Check_Root
     Prepare_System
     Set_Dir
-    # Install_Docker
-    # Install_Compose
+    Install_Docker
+    Install_Compose
     Set_Port
     Set_Firewall
     Set_Username
