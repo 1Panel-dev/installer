63291bdb9ca48eee4e9b765ca7b613e39cd5b21a
JB-SelfCompany <github@shd.company>
2024-12-25 08:48:57 +0000

    Russian language support has been added. (#30)

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ CURRENT_DIR=$(
 
 LANG_FILE=".selected_language"
 LANG_DIR="$CURRENT_DIR/lang"
-AVAILABLE_LANGS=("en" "zh" "fa" "pt-BR")
+AVAILABLE_LANGS=("en" "zh" "fa" "pt-BR" "ru")
 
 declare -A LANG_NAMES
-LANG_NAMES=( ["en"]="English" ["zh"]="Chinese  中文(简体)" ["fa"]="Persian" ["pt-BR"]="Português (Brasil)" )
+LANG_NAMES=( ["en"]="English" ["zh"]="Chinese  中文(简体)" ["fa"]="Persian" ["pt-BR"]="Português (Brasil)" ["ru"]="Русский" )
 
 if [ -f "$CURRENT_DIR/$LANG_FILE" ]; then
     selected_lang=$(cat "$CURRENT_DIR/$LANG_FILE")
